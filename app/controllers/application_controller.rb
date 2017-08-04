7da01f94732aa67f7e8a50a11625a131f63d7dfe
Mateusz Skrobis <skrobis.mateusz@gmail.com>
2017-08-04 14:00:46 +0200

    Add users resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   include JSONAPI::ActsAsResourceController
+  include ActionController::RequestForgeryProtection
 
   protect_from_forgery with: :null_session
 end
