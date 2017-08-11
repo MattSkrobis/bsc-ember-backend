b9bd157618e6d8b10801059eaa1ecdc8aca755f4
MattSkrobis <skrobismateusz@gmail.com>
2017-08-11 08:37:36 +0200

    Configure rspec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,33 @@
+ENV["RAILS_ENV"] ||= 'test'
+if ENV['WITH_SIMPLE_COV']
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'email_spec'
+ActiveRecord::Migration.maintain_test_schema!
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+# Delayed::Worker.delay_jobs = false
+RSpec.configure do |config|
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
+  config.include(FactoryGirl::Syntax::Methods)
+  config.use_transactional_fixtures = false
+  config.infer_base_class_for_anonymous_controllers = false
+  config.infer_spec_type_from_file_location!
+  I18n.enforce_available_locales = false
+  config.order = 'random'
+  config.render_views = true
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+    ActionMailer::Base.deliveries.clear
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
