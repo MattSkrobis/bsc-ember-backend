8b2ecf7ad52f66af0fa08d50f1fcfcbd962ebded
Mateusz Skrobis <skrobis.mateusz@gmail.com>
2017-08-18 14:16:54 +0200

    Setup models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810131822) do
+ActiveRecord::Schema.define(version: 20170818113159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.boolean "visible"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -25,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170810131822) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
