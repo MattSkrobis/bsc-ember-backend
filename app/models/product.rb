ae390df5c154550b0cdfef365d275d4e560f1ffb
MattSkrobis <skrobismateusz@gmail.com>
2017-08-22 14:26:42 +0200

    Add a bit more sophisticated filter

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  include PgSearch
   belongs_to :category
+  pg_search_scope :search, :against => :name, :using => {:tsearch => {:prefix => true}}
 end
--- app/resources/product_resource.rb
+++ app/resources/product_resource.rb
@@ -1,6 +1,8 @@
 class ProductResource < JSONAPI::Resource
   attributes :name, :description, :sku, :availability, :currency, :price, :quantity
 
-  filter :availability
-  filter :name
+    filter :availability
+    filter :name, apply: ->(records, value, _options) {
+      value[0] ? records.search(value[0]) : records
+    }
 end
